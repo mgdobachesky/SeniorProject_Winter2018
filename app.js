ba13a08686460177ac6a156a710a73f6241de26d
Michael Dobachesky <mike91doby@gmail.com>
2018-01-23 17:44:05 -0500

    more favicon tweaks

--- app.js
+++ app.js
@@ -25,15 +25,15 @@ var routesApi = require('./app_api/routes/index');
 // Create the main Express application
 var app = express();
 
-// Set the application icon
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
 // Set the application view engine
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.set('view engine', 'pug');
 
 // Set the Application local directories
 app.locals.basedir = path.join(__dirname, 'public');
+
+// Set the application icon
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
 // Configure the Express Session middleware
 app.use(session({
